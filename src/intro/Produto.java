46ac0e103a423b2d338a9327e046aa4f16438dea
TheRecruta <211671592+A-Belfort@users.noreply.github.com>
2025-08-21 19:59:47 -0300

    classe de produto

--- src/intro/Produto.java
+++ src/intro/Produto.java
@@ -1,5 +1,25 @@
 package intro;
 
+// Classe de produto, outras vão herdar dela 
 public class Produto {
+    // Todo produto tem um nome e preço
+    private final String nome;
+    private final Float preco;
+
+    // Inicializa um objeto de produto
+    public Produto(String nome, Float preco) { 
+        this.nome = nome;
+        this.preco = preco;
+    };
+
+    // Retorna do nome do produto
+    public String getNome() {
+        return nome;
+    }
+
+    // Retorna o preço do produto
+    public float getPreco() {
+        return preco;
+    }
 
 }
