d5022f4ee7604100924411474b7037c0b4946623
A-Belfort <arthur.ferreira0020016111@edu.pe.senac.br>
2025-08-21 09:57:56 -0300

    main de teste

--- src/intro/app.java
+++ src/intro/app.java
@@ -1,9 +1,24 @@
 package intro;
 
+import java.util.Scanner;
+
 public class app {
 
 	public static void main(String[] args) {
+		// Entrada do usuário
+		// Diferente de Python, é necessário instanciar um objeto chamado Scanner
+		Scanner input = new Scanner(System.in);
+		
 		System.out.println("Testando");
-	}
+		System.out.println("Digite algo: ");
 
+		// Variável que vai receber a entrada do usuário
+		String inputText = input.nextLine();
+
+		// Printa o que o usuário escreveu
+		System.out.println("Vc disse: " + inputText);
+		
+		// Fecha o scanner para não vazar memória
+		input.close();
+	}
 }
