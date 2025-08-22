d328b22119108d7a5dda56a5ebd03f88af1babd8
A-Belfort <arthur.ferreira0020016111@edu.pe.senac.br>
2025-08-22 09:03:38 -0300

    Update app.java

--- src/intro/app.java
+++ src/intro/app.java
@@ -1,14 +1,20 @@
 package intro;
 
 import java.util.Scanner;
+import java.util.List;
 
 public class App {
 
 	public static void main(String[] args) {
-		Produto livro = new Livro("Livro",25.50, "Anonimo", 2025);
-		Produto ebook = new Ebook("Livro: E", 10.15, "Anonimo2", 2025, 10.77);
-		Produto audiobook = new Audiobook("LivroAudio", 5.99, "Anonimo3", 2025, 120);
-		Produto papelaria = new ItemPapelaria("Papelaria", 4.99, "PapelPlus", "É papel!");
+		
+		
+		List<Produto> prateleira = List.of(
+			new Livro("Livro",25.50, "Anonimo", 2025),
+			new Ebook("Livro: E", 10.15, "Anonimo2", 2025, 10.77),
+			new Audiobook("LivroAudio", 5.99, "Anonimo3", 2025, 120),
+			new ItemPapelaria("Papelaria", 4.99, "PapelPlus", "É papel!")
+		);
+		
 		// Entrada do usuário
 		// Diferente de Python, é necessário instanciar um objeto chamado Scanner
 		//Scanner input = new Scanner(System.in);
@@ -23,14 +29,11 @@ public class App {
 		System.out.println("Bem-vindo a livraria digital!");
 		System.out.println("----------------");
 		System.out.println("Produto:\n");
-		livro.exibirDetalhes();
-		System.out.println("---");
-		ebook.exibirDetalhes();
-		System.out.println("---");
-		audiobook.exibirDetalhes();
-		System.out.println("---");
-		papelaria.exibirDetalhes();
-		System.out.println("---");
+
+		for (Produto p : prateleira) {
+			p.exibirDetalhes();
+			System.out.println("---");
+		};
 		
 		// Fecha o scanner para não vazar memória
 		//input.close();
